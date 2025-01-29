eecdab1e462af7e12695adc5b23f5554185ef354
Braynin <brayhannb@gmail.com>
2025-01-29 18:04:54 -0500

    creando arrays

---
+++ src/assets/ImprentaProducts.js
@@ -0,0 +1,115 @@
+const ImprentaProducts = [
+  {
+    title: "Papeleria",
+    image: "bolsas.jpg",
+    description: "Bolsas de diferentes tamaños y colores.",
+    products: [
+      {
+        title: "Bolsa 1",
+        image: "bolsa1.jpg",
+        description: "Bolsa de diferentes tamaños y colores.",
+        caracteristicas: [
+          "Caracteristica 1",
+          "Caracteristica 2",
+          "Caracteristica 3",
+        ],
+      },
+      {
+        title: "Bolsa 2",
+        image: "bolsa2.jpg",
+        description: "Bolsa de diferentes tamaños y colores.",
+        caracteristicas: [
+          "Caracteristica 1",
+          "Caracteristica 2",
+          "Caracteristica 3",
+        ],
+      },
+      {
+        title: "Bolsa 3",
+        image: "bolsa3.jpg",
+        description: "Bolsa de diferentes tamaños y colores.",
+        caracteristicas: [
+          "Caracteristica 1",
+          "Caracteristica 2",
+          "Caracteristica 3",
+        ],
+      },
+    ],
+  },
+  {
+    title: "Merchandising",
+    image: "cajas.jpg",
+    description: "Cajas de diferentes tamaños y colores.",
+    products: [
+      {
+        title: "Bolsa 1",
+        image: "bolsa1.jpg",
+        description: "Bolsa de diferentes tamaños y colores.",
+        caracteristicas: [
+          "Caracteristica 1",
+          "Caracteristica 2",
+          "Caracteristica 3",
+        ],
+      },
+      {
+        title: "Bolsa 2",
+        image: "bolsa2.jpg",
+        description: "Bolsa de diferentes tamaños y colores.",
+        caracteristicas: [
+          "Caracteristica 1",
+          "Caracteristica 2",
+          "Caracteristica 3",
+        ],
+      },
+      {
+        title: "Bolsa 3",
+        image: "bolsa3.jpg",
+        description: "Bolsa de diferentes tamaños y colores.",
+        caracteristicas: [
+          "Caracteristica 1",
+          "Caracteristica 2",
+          "Caracteristica 3",
+        ],
+      },
+    ],
+  },
+  {
+    title: "Impresiones",
+    image: "envolturas.jpg",
+    description: "Bolsas de diferentes tamaños y colores.",
+    products: [
+      {
+        title: "Bolsa 1",
+        image: "bolsa1.jpg",
+        description: "Bolsa de diferentes tamaños y colores.",
+        caracteristicas: [
+          "Caracteristica 1",
+          "Caracteristica 2",
+          "Caracteristica 3",
+        ],
+      },
+      {
+        title: "Bolsa 2",
+        image: "bolsa2.jpg",
+        description: "Bolsa de diferentes tamaños y colores.",
+        caracteristicas: [
+          "Caracteristica 1",
+          "Caracteristica 2",
+          "Caracteristica 3",
+        ],
+      },
+      {
+        title: "Bolsa 3",
+        image: "bolsa3.jpg",
+        description: "Bolsa de diferentes tamaños y colores.",
+        caracteristicas: [
+          "Caracteristica 1",
+          "Caracteristica 2",
+          "Caracteristica 3",
+        ],
+      },
+    ],
+  },
+];
+
+export default ImprentaProducts;
---
+++ src/assets/PackagingProducts.js
@@ -0,0 +1,115 @@
+const PackagingProducts = [
+  {
+    title: "Bolsas",
+    image: "bolsas.jpg",
+    description: "Bolsas de diferentes tamaños y colores.",
+    products: [
+      {
+        title: "Bolsa 1",
+        image: "bolsa1.jpg",
+        description: "Bolsa de diferentes tamaños y colores.",
+        caracteristicas: [
+          "Caracteristica 1",
+          "Caracteristica 2",
+          "Caracteristica 3",
+        ],
+      },
+      {
+        title: "Bolsa 2",
+        image: "bolsa2.jpg",
+        description: "Bolsa de diferentes tamaños y colores.",
+        caracteristicas: [
+          "Caracteristica 1",
+          "Caracteristica 2",
+          "Caracteristica 3",
+        ],
+      },
+      {
+        title: "Bolsa 3",
+        image: "bolsa3.jpg",
+        description: "Bolsa de diferentes tamaños y colores.",
+        caracteristicas: [
+          "Caracteristica 1",
+          "Caracteristica 2",
+          "Caracteristica 3",
+        ],
+      },
+    ],
+  },
+  {
+    title: "Cajas",
+    image: "cajas.jpg",
+    description: "Cajas de diferentes tamaños y colores.",
+    products: [
+      {
+        title: "Bolsa 1",
+        image: "bolsa1.jpg",
+        description: "Bolsa de diferentes tamaños y colores.",
+        caracteristicas: [
+          "Caracteristica 1",
+          "Caracteristica 2",
+          "Caracteristica 3",
+        ],
+      },
+      {
+        title: "Bolsa 2",
+        image: "bolsa2.jpg",
+        description: "Bolsa de diferentes tamaños y colores.",
+        caracteristicas: [
+          "Caracteristica 1",
+          "Caracteristica 2",
+          "Caracteristica 3",
+        ],
+      },
+      {
+        title: "Bolsa 3",
+        image: "bolsa3.jpg",
+        description: "Bolsa de diferentes tamaños y colores.",
+        caracteristicas: [
+          "Caracteristica 1",
+          "Caracteristica 2",
+          "Caracteristica 3",
+        ],
+      },
+    ],
+  },
+  {
+    title: "Envolturas para alimentos",
+    image: "envolturas.jpg",
+    description: "Bolsas de diferentes tamaños y colores.",
+    products: [
+      {
+        title: "Bolsa 1",
+        image: "bolsa1.jpg",
+        description: "Bolsa de diferentes tamaños y colores.",
+        caracteristicas: [
+          "Caracteristica 1",
+          "Caracteristica 2",
+          "Caracteristica 3",
+        ],
+      },
+      {
+        title: "Bolsa 2",
+        image: "bolsa2.jpg",
+        description: "Bolsa de diferentes tamaños y colores.",
+        caracteristicas: [
+          "Caracteristica 1",
+          "Caracteristica 2",
+          "Caracteristica 3",
+        ],
+      },
+      {
+        title: "Bolsa 3",
+        image: "bolsa3.jpg",
+        description: "Bolsa de diferentes tamaños y colores.",
+        caracteristicas: [
+          "Caracteristica 1",
+          "Caracteristica 2",
+          "Caracteristica 3",
+        ],
+      },
+    ],
+  },
+];
+
+export default PackagingProducts;
